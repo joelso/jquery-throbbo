851135ab2003675e64a8aaa0ff39cd0f57dfc265
joelso <joel.soderstrom@gmail.com>
2010-10-13 09:41:47 +0200

    Docs

--- jquery.throbbo.js
+++ jquery.throbbo.js
@@ -1,75 +1,78 @@
 (function($){
-	
-	var defaultOpts = {
-	   duration:1000,
-	   complete: function() {},
-	   cssClass: 'throbber',
-	   mode: 'wrap',                    // wrap | overlay
-	   container: null,
-	   image:'throbber.gif'
-	};
-	
-	var debug = true;
-	
-	function _log(msg) {
-        if(debug && typeof console !== 'undefined') console.log(msg); 
+
+    var defaultOpts = {
+        // How long throbber will show, in ms
+        duration: 1000,
+        
+        // Callback when throbber has shown for given duration
+        complete: function(){},
+        
+        cssClass: 'throbber',
+        
+        // In what way throbber will show
+        mode: 'overlay',
+        
+        // Path to image, relative to this script or absolute
+        image: 'throbber.gif'
+    };
+    
+    var debug = true;
+    
+    function _log(msg){
+        if (debug && typeof console !== 'undefined') 
+            console.log(msg);
     }
-	
-	function _wrap(el, opts) {
-		throw('Not yet implemented!');
-
-		var $el;
-		
-		if(opts.container) {
-			$el = $(opts.container);
-		} else {
-    		$el = $(el);
-		}
-		
-		/*$el.wrap(throbberHtml);
-
-		$el.hide(0).delay(opts.duration).show(0).queue(function() {
-            $el.unwrap();
-			$el.dequeue();
-		});*/
-	}
-	
-	function _overlay(el, opts) {
-        var $el, $overlay;
-		
-		$el = $(el);
-		
-		$el.prepend('<div class="throbberOverlay"></div>');
-		
-		$overlay = $el.find('.throbberOverlay');
-		
-        $overlay.css({
+    
+    function _wrap(el, opts){
+        throw ('Not yet implemented!');
+        
+        var $el = $(el);
+        
+        /*$el.wrap(throbberHtml);
+        
+         $el.hide(0).delay(opts.duration).show(0).queue(function() {
+        
+         $el.unwrap();
+        
+         $el.dequeue();
+        
+         });*/
+        
+    }
+    
+    function _overlay(el, opts){
+        var $el = $(el).prepend('<div class="throbberOverlay"></div>');
+        
+        var $overlay = $el.find('.throbberOverlay').css({
             'position': 'absolute',
             'background': '#f1f1f1 url(' + opts.image + ') center center no-repeat'
         })
-		.width( $el.width() ).height( $el.height() )
-
+		
+		.width($el.width()).height($el.height())
+		
 		.delay(opts.duration).queue(function(){
             $overlay.remove();
             $overlay.dequeue();
+			opts.complete();
         });
     }
-	
+    
     $.fn.throbbo = function(opts){
-        
-        return this.each(function() {
+    
+        return this.each(function(){
             _log('[jquery.throbbo] ' + this);
             opts = $.extend({}, defaultOpts, opts);
-			
-			if(opts.mode === 'wrap') {
-				_wrap(this, opts);
-			} 
-			else if(opts.mode === 'overlay') {
-				_overlay($(opts.container), opts);
-			}
+            
+            if (opts.mode === 'wrap') {
+                _wrap(this, opts);
+            }
+            else 
+                if (opts.mode === 'overlay') {
+                    _overlay($(opts.container), opts);
+                }
         });
         
     };
-	
+    
 })(jQuery);
 
